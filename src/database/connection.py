17fe952dffe25357005a0c01f09bf51885f5b8c3
elijahwenndt <elijah.wenndt@student.awesomeinc.org>
2022-11-03 18:26:21 +0000

    new working model

--- src/database/connection.py
+++ src/database/connection.py
@@ -29,11 +29,112 @@ def execute_query(query, params=None):
     except OSError as e:
         print(f"The error '{e}' occurred or the hero name is already taken")
 
+# NO TOUCHIE
+
+def create():
+    creation = input('WHAT WOULD YOU LIKE TO CREATE?(new hero/add ability): ')
+    if creation == 'new hero':
+        hero_name = input('WHAT IS THE NAME OF THE HERO THAT IS JOINED THE AGENCY?: ')
+        hero_about = input('WHAT DO YOU KNOW ABOUT THE HERO?: ')
+        hero_bio = input('WHAT IS THE HEROES BACKSTORY?: ')
+        query = """
+            INSERT INTO heroes (name, about_me, biography)
+            VALUES (%s, %s, %s);
+        """
+        execute_query(query, [hero_name, hero_about, hero_bio])
+        print(f"THE DIRECTOR WELCOMES {hero_name} INTO THE AGENCY")
+        print('QUERY COMPLETE, RETURNING TO ACCESS PORT')
+        basic_info()
+    elif creation == 'add ability':
+        hero_id = input('WHAT IS THE NUMERICAL IDENTIFICATION OF THE HERO?: ')
+        ability_type = input('WHAT ABILITIES DOES THE HERO POSSES?(REFERENCE ABILITY_TYPE TABLE): ')
+        query = """
+            INSERT INTO abilities (hero_id, ability_type_id)
+            VALUES (%s, %s);
+        """
+        execute_query(query, [hero_id, ability_type])
+        print(f"HERO'S ABILITY ADDED")
+        print('QUERY COMPLETE, RETURNING TO ACCESS PORT')
+        director_action()
+
+def read():
+    y = input('WOULD YOU LIKE TO SEE A CURRENT LIST OF ACTIVE HEROES? (y/n): ')
+    if y == 'y':
+        query = """
+            SELECT heroes.name 
+            FROM heroes
+        """
+        list_of_heroes = execute_query(query).fetchall()
+        for record in list_of_heroes:
+                print('')
+                print(record[0])
+    else: print('OK')
+    z = input('WHAT INFORMATION WOULD YOU LIKE TO KNOW?(abilities): ')
+    if z == 'abilities':
+        query = """
+            SELECT heroes.name, ability_types.name 
+            FROM heroes
+            JOIN abilities ON heroes.id = abilities.hero_id
+            JOIN ability_types ON ability_types.id = abilities.ability_type_id 
+        """
+        list_of_heroes = execute_query(query).fetchall()
+        for record in list_of_heroes:
+                print('')
+                print(record[0] + ' ' + record[1])
+    else: print('INVALID, EXIT PROTOCOL INITIATED')
+    print('QUERY COMPLETE, RETURNING TO ACCESS PORT')
+    director_action()
+
+def update():
+    update_info = input('WHAT WOULD YOU LIKE TO CHANGE?(name): ')
+    if update_info == 'name':
+        which_hero = input('WHICH HERO IS CHANGING THERE IDENTITY?(REFERENCE HERO(ID) TABLE): ')
+        new_hero_name = input('WHAT IS THE NEW IDENTITY OF THE HERO?: ')
+        query = """
+            UPDATE heroes
+            SET name = %s
+            WHERE id = %s
+        """
+        execute_query(query, [new_hero_name, which_hero])
+        print(f"THE AGENCY NOW RECOGNIZES {new_hero_name}")
+        print('QUERY COMPLETE, RETURNING TO ACCESS PORT')
+        director_action()
+
+def delete():
+    hero_death = input('I AM SORRY TO HEAR THAT DIRECTOR, WHICH HERO HAS DIED?: ')
+    query = """
+        DELETE FROM heroes WHERE name = %s
+    """
+    execute_query(query, [hero_death,])
+    print(f"{hero_death} WILL BE REMEMBERED")
+    print('QUERY COMPLETE, RETURNING TO ACCESS PORT')
+    director_action()
+
+def director_action():
+    crud_input = input('WHAT WOULD YOU LIKE TO DO TODAY DIRECTOR?(create/read/update/delete): ')
+    if crud_input == 'create':
+        create()
+    elif crud_input == 'read':
+        read()
+    elif crud_input == 'update':
+        update()
+    elif crud_input == 'delete':
+        delete()
+
 def main_terminal():
     director_input = input('WHO IS ACCESSING THE TERMINAL: ')
     if director_input == 'director':
-        print('WELCOME BACK DIRECTOR')
-        crud_input = input('WHAT WOULD YOU LIKE TO DO TODAY DIRECTOR?(create/read/update/delete)')
+        director()
+    else:
+        print('ACCESS DENIED')
+        i = 1
+        while i < 11:
+            print('CONTACTING AUTHORITIES')
+            i += 1
+        print('PORT TERMINATED')
+main_terminal() 
+
+
 
 # def basic_info():
 #     x = input('WHO IS ACCESSING THE TERMINAL: ')
@@ -118,7 +219,3 @@ def main_terminal():
 #     else: print('ACCESS DENIED, CALLING AUTHORITIES')
 # basic_info()
 
-# SELECT heroes.name, ability_types.name 
-#                 FROM heroes
-#                 JOIN abilities ON heroes.id = abilities.hero_id
-#                 JOIN ability_types ON ability_types.id = abilities.ability_type_id
