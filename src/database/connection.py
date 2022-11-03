f5effcfa365bc4b430c77bf3d2c0a50a7bb17a99
elijahwenndt <elijah.wenndt@student.awesomeinc.org>
2022-11-03 17:29:41 +0000

    MVP met, working on splitting up functions

--- src/database/connection.py
+++ src/database/connection.py
@@ -29,62 +29,94 @@ def execute_query(query, params=None):
     except OSError as e:
         print(f"The error '{e}' occurred or the hero name is already taken")
 
-# DO NOT TOUCH FILE ABOVE
+def main_terminal():
+    director_input = input('WHO IS ACCESSING THE TERMINAL: ')
+    if director_input == 'director':
+        print('WELCOME BACK DIRECTOR')
+        crud_input = input('WHAT WOULD YOU LIKE TO DO TODAY DIRECTOR?(create/read/update/delete)')
 
-# def select_all():
-#     query = """
-#         SELECT * from heroes
-#     """
-
-#     list_of_heroes = execute_query(query).fetchall()
-#     print(list_of_heroes)
-#     for record in list_of_heroes:
-#         print(record[1])
-
-# select_all()
-
-def basic_info():
-    x = input('WHO IS ACCESSING THE TERMINAL: ')
-    print('hello ' + x)
-    if x == 'director':
-        a = input('WHAT WOULD YOU LIKE TO DO DIRECTOR?(CREATE/READ): ')
-        if a == 'read':
-            y = input('WOULD YOU LIKE TO SEE A CURRENT LIST OF ACTIVE HEROES? (y/n): ')
-            if y == 'y':
-                query = """
-                    SELECT heroes.name 
-                    FROM heroes
-                """
-                list_of_heroes = execute_query(query).fetchall()
-                for record in list_of_heroes:
-                        print('')
-                        print(record[0])
-            else: print('OK')
-            z = input('WHAT INFORMATION WOULD YOU LIKE TO KNOW?(abilities): ')
-            if z == 'abilities':
-                query = """
-                    SELECT heroes.name, ability_types.name 
-                    FROM heroes
-                    JOIN abilities ON heroes.id = abilities.hero_id
-                    JOIN ability_types ON ability_types.id = abilities.ability_type_id 
-                """
-                list_of_heroes = execute_query(query).fetchall()
-                for record in list_of_heroes:
-                        print('')
-                        print(record[0] + ' ' + record[1])
-            else: print('INVALID')
-        elif a == 'create':
-            hero_name = print('WHAT IS THE NAME OF THE HERO THAT IS JOINED THE AGENCY?: ')
-            hero_about = print('WHAT DO YOU KNOW ABOUT THE HERO?: ')
-            hero_bio = print('WHAT IS THE HEROES BACKSTORY?: ')
-            query = """
-                INSERT INTO heroes (name, about_me, biography)
-                VALUES (%s, %s, %s);
-            """
-            execute_query(query, [hero_name, hero_about, hero_bio])
-            print(f"THE DIRECTOR WELCOMES {hero_name} INTO THE AGENCY")
-    else: print('ACCESS DENIED, CALLING AUTHORITIES')
-basic_info()
+# def basic_info():
+#     x = input('WHO IS ACCESSING THE TERMINAL: ')
+#     print('hello ' + x)
+#     if x == 'director':
+#         a = input('WHAT WOULD YOU LIKE TO DO DIRECTOR?(create/read/update/delete): ')
+#         if a == 'read':
+#             y = input('WOULD YOU LIKE TO SEE A CURRENT LIST OF ACTIVE HEROES? (y/n): ')
+#             if y == 'y':
+#                 query = """
+#                     SELECT heroes.name 
+#                     FROM heroes
+#                 """
+#                 list_of_heroes = execute_query(query).fetchall()
+#                 for record in list_of_heroes:
+#                         print('')
+#                         print(record[0])
+#             else: print('OK')
+#             z = input('WHAT INFORMATION WOULD YOU LIKE TO KNOW?(abilities): ')
+#             if z == 'abilities':
+#                 query = """
+#                     SELECT heroes.name, ability_types.name 
+#                     FROM heroes
+#                     JOIN abilities ON heroes.id = abilities.hero_id
+#                     JOIN ability_types ON ability_types.id = abilities.ability_type_id 
+#                 """
+#                 list_of_heroes = execute_query(query).fetchall()
+#                 for record in list_of_heroes:
+#                         print('')
+#                         print(record[0] + ' ' + record[1])
+#             else: print('INVALID, EXIT PROTOCOL INITIATED')
+#             print('QUERY COMPLETE, RETURNING TO ACCESS PORT')
+#             basic_info()
+#         elif a == 'create':
+#             creation = input('WHAT WOULD YOU LIKE TO CREATE?(new hero/new ability): ')
+#             if creation == 'new hero':
+#                 hero_name = input('WHAT IS THE NAME OF THE HERO THAT IS JOINED THE AGENCY?: ')
+#                 hero_about = input('WHAT DO YOU KNOW ABOUT THE HERO?: ')
+#                 hero_bio = input('WHAT IS THE HEROES BACKSTORY?: ')
+#                 query = """
+#                     INSERT INTO heroes (name, about_me, biography)
+#                     VALUES (%s, %s, %s);
+#                 """
+#                 execute_query(query, [hero_name, hero_about, hero_bio])
+#                 print(f"THE DIRECTOR WELCOMES {hero_name} INTO THE AGENCY")
+#                 print('QUERY COMPLETE, RETURNING TO ACCESS PORT')
+#                 basic_info()
+#             elif creation == 'new ability':
+#                 hero_id = input('WHAT IS THE NUMERICAL IDENTIFICATION OF THE HERO?: ')
+#                 ability_type = input('WHAT ABILITIES DOES THE HERO POSSES?(REFERENCE ABILITY_TYPE TABLE): ')
+#                 query = """
+#                     INSERT INTO abilities (hero_id, ability_type_id)
+#                     VALUES (%s, %s);
+#                 """
+#                 execute_query(query, [hero_id, ability_type])
+#                 print(f"HERO'S ABILITY ADDED")
+#                 print('QUERY COMPLETE, RETURNING TO ACCESS PORT')
+#                 basic_info()
+#         elif a == 'update':
+#             update_info = input('WHAT WOULD YOU LIKE TO CHANGE?(name): ')
+#             if update_info == 'name':
+#                 which_hero = input('WHICH HERO IS CHANGING THERE IDENTITY?(REFERENCE HERO(ID) TABLE): ')
+#                 new_hero_name = input('WHAT IS THE NEW IDENTITY OF THE HERO?: ')
+#                 query = """
+#                     UPDATE heroes
+#                     SET name = %s
+#                     WHERE id = %s
+#                 """
+#                 execute_query(query, [new_hero_name, which_hero])
+#                 print(f"THE AGENCY NOW RECOGNIZES {new_hero_name}")
+#                 print('QUERY COMPLETE, RETURNING TO ACCESS PORT')
+#                 basic_info()
+#         elif a == 'delete':
+#             hero_death = input('I AM SORRY TO HEAR THAT DIRECTOR, WHICH HERO HAS DIED?: ')
+#             query = """
+#                 DELETE FROM heroes WHERE name = %s
+#             """
+#             execute_query(query, [hero_death,])
+#             print(f"{hero_death} WILL BE REMEMBERED")
+#             print('QUERY COMPLETE, RETURNING TO ACCESS PORT')
+#             basic_info()
+#     else: print('ACCESS DENIED, CALLING AUTHORITIES')
+# basic_info()
 
 # SELECT heroes.name, ability_types.name 
 #                 FROM heroes
